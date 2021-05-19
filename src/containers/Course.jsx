a3d7a11e9ea99c7d75146ecc9e04412140c9b5f3
Rogelio Medina <rogelio.medina.cs@gmail.com>
2021-05-18 19:00:50 -0500

    info section styles for horizontal phone fixed

---
+++ src/containers/Course.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/containers/Course.scss";
+
+const Course = () => {
+  return (
+    <div className="course-id">
+      <div></div>
+    </div>
+  );
+};
+
+export default Course;
